24fe7b4f1dd8f470313dda9173a43f113285da13
saurabhpathak <saurabhpathak072@gmail.com>
2025-04-19 00:43:36 +0530

    rotate the array to right left in java

--- src/ArrayRotation.java
+++ src/ArrayRotation.java
@@ -1,5 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ArrayRotation {
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter '1' to rotate Right and '0' to rotate Left: ");
+        int dir = sc.nextInt();
+        System.out.println("Enter number of rotations: ");
+        int x = sc.nextInt();
+
+        int[] A = {1, 2, 3, 4, 5, 6, 7, 8};
+        int len = A.length ;
+        int[] B = new int[len];
+
+        if(dir == 1){
+            for (int i = 0; i < len; i++) {
+                B[(i+x)%len] = A[i];
+            }
+        }
+
+        else{
+            for (int i = 0; i < len; i++) {
+                B[(i-x+len)%len] = A[i];
+            }
+        }
+
+        sc.close();
+        System.out.println(Arrays.toString(B));
     }
 }
